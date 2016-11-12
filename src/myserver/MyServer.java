2f9244835ca87290e1ea73f9b60123b9bed3a9fd
KuKuKanz <tung.coder1992@gmail.com>
2016-11-12 19:23:20 +0700

    My first handling with java
    Sample script to open a server on java

---
+++ src/myserver/MyServer.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myserver;
+
+import com.sun.org.apache.bcel.internal.generic.AALOAD;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * s
+ *
+ * @author kukulkan
+ */
+public class MyServer {
+
+    /**
+     */
+    static public void OpenServer() throws IOException {
+        ServerSocket server = new ServerSocket(8080);
+            System.out.println("Open server ......");
+
+        try {
+            Socket client = server.accept();
+            
+            System.out.println("Open server successfully");
+            
+            BufferedReader bufReader = new BufferedReader(new  InputStreamReader( client.getInputStream()));
+            
+            String inputLine = "";
+            
+            while ((inputLine = bufReader.readLine()) != null){
+                System.out.print(inputLine);
+            }
+            
+            
+        }catch (IOException e){
+            System.out.print(e);
+        }
+
+    }
+
+}
